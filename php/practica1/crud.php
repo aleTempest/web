67429ac24a43399a3fb48a7e8585c2d56952b338
ale <2030406@upv.edu.mx>
2024-03-04 08:15:13 -0600

    notengoxampp

--- php/practica1/crud.php
+++ php/practica1/crud.php
@@ -1,2 +1,16 @@
 <?
 include 'db.php'
+// alta de alumnos
+if (isset($_POST[alta_alumno])) {
+	// Obtener los datos del formulario
+	$matricula	= $_POST['matricula'];
+	$nombre			= $_POST['nombre'];
+	$edad				= $_POST['edad'];
+	$email			= $_POST['email'];
+	$id_carrera = $_POST['id_carrera'];
+
+	// Insert a la tabla
+	$sql = "INSERT INTO alumnos (matricula,nombre,edad,email,id_carrera) 
+		VALUES ('$matricula,$nombre,$edad,$email,$id_carrera')";
+	$res = $conn->query($sql)
+}
