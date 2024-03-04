af4c2ba85c5e75dcd2708c75fd26d6194af5bb7b
ale <2030406@upv.edu.mx>
2024-03-04 08:02:10 -0600

    test php

---
+++ php/practica1/db.php
@@ -0,0 +1,12 @@
+<?
+// Conexion a la base de datos
+$server_name = "64.23.175.73"
+$username = "ale"
+$password = "elaina"
+$db_name	= "universidad"
+
+$conn = new msqli($server_name,$username,$password, $db_name)
+if ($conn->connect_error) {
+	die("Conexión fallida" . $conn->connect_error)
+}
+> 
