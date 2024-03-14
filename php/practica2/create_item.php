d25046d247cf45891657901532be5bfb5c6394c2
ale <2030406@upv.edu.mx>
2024-03-13 23:37:07 -0600

    navbar fix

--- php/practica2/create_item.php
+++ php/practica2/create_item.php
@@ -12,6 +12,26 @@ $cat_dao = createCatalogDao();
 		<title>Catálogo</title>
 	</head>
 	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="index.html">
+				<img src="img/upvlogo.png" alt="logo" width="60" height="60">
+					Index
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="vehicle_list.php">Vehículos <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="item_list.php">Catálogo</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
 		<div class="container">
 			<h2>Añadir servicio al catálogo</h2>
 			<form action="crud.php" method="post">
--- php/practica2/update_item.php
+++ php/practica2/update_item.php
@@ -15,6 +15,26 @@ $item = $cat_dao->getItem($id);
 		<title>Edit item</title>
 	</head>
 	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="index.html">
+				<img src="img/upvlogo.png" alt="logo" width="60" height="60">
+					Index
+			</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="vehicle_list.php">Vehículos <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="item_list.php">Catálogo</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
 		<div class="container">
 			<h2>Editar catálogo</h2>
 			<form action="crud.php" method="post">
