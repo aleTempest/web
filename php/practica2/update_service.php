3db4201d313902a508742ea30252f0b651f3db0c
ale <aguxtonh@gmail.com>
2024-03-19 18:59:17 -0600

    navbar

--- php/practica2/update_service.php
+++ php/practica2/update_service.php
@@ -21,6 +21,27 @@ $actual_row = $conn->query($sql)->fetch_assoc(); // resultado de la consulta de 
     <title>Editar Servicio</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="index.html">
+        <img src="img/upvlogo.png" alt="logo" width="60" height="60">
+        Index
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="vehicle_list.php">Vehículos <span
+                            class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="item_list.php">Catálogo</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <div class="container">
     <form action="crud.php" method="post">
         <input type="hidden" name="s_id" value="<?php echo $id?>">
