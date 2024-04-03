3ce332cb12eb5d08b8f8f2b2880bf428d5cd87b5
ale <ale.edgedancer@gmail.com>
2024-04-03 08:20:04 -0600

    altas

--- php/examen-u2/index.php
+++ php/examen-u2/index.php
@@ -9,12 +9,15 @@ $res = $conn->query($sql);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<title>Libros</title>
 </head>
 
 <body>
+	<a href="add_book.php">Alta de libros</a>
+	<a href="add_editorial.php">Alta de editorial</a>
 	<h1>Libros</h1>
-	<table>
+	<table class="table table-hover">
 		<thead>
 			<tr>
 				<th>Nombre</th>
@@ -39,7 +42,7 @@ $res = $conn->query($sql);
 		</tbody>
 	</table>
 	<h1>Editoriales</h1>
-	<table>
+	<table class="table table-hover">
 		<thead>
 			<tr>
 				<th>Nombre</th>
